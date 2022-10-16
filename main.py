2d704eeed74e226f9cb8f99e3bfb071849440f6e
lucabln05 <luca.bellon@icloud.com>
2022-10-16 19:11:33 +0200

    Version2.3

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import playsound            #pip install playsound; https://www.delftstack.com/d
 import time                 #https://docs.python.org/3/library/time.html
 import os                   #https://rdrr.io/cran/installr/man/os.shutdown.html
 import threading            #https://docs.python.org/3/library/threading.html
-import win32gui, win32con   #https://www.anycodings.com/1questions/662705/python-console-fullscreen-maybe-using-ossystem
 
 
 
@@ -43,9 +42,6 @@ try:
     def music():
         playsound.playsound('music.mp3')
         print('ENJOY')
-        hwnd = win32gui.GetForegroundWindow()
-        win32gui.ShowWindow(hwnd, win32con.SW_MAXIMIZE)
-
 
     #COUNT FROM 15 to 1, than shutdown the pc
     def consol_output():
