691a7488d5c3d5a616c09ad3d7c51bd37dff3615
lucabln05 <luca.bellon@icloud.com>
2022-10-16 16:09:42 +0200

    Version0.2

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
-import winsound
+import playsound            #pip install playsound; https://www.delftstack.com/de/howto/python/python-play-mp3/
+import time
 
-winsound.PlaySound('Out')
+
+try:
+
+    #playsound.playsound('music.mp3')
+
+    print("Shutdown PC in:")
+
+
+    count = 0
+
+    while count < 13:
+        time.sleep(1)
+        print(13 - count)
+        count = count + 1
+
+
+except Exception as err:
+    print(err)
+
+
+
+
