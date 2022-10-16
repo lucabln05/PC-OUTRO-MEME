895c9706e5f1638a9e9a618c45c56523bdd022f1
lucabln05 <luca.bellon@icloud.com>
2022-10-16 16:24:09 +0200

    Version1.0

--- main.py
+++ main.py
@@ -1,20 +1,36 @@
+
 import playsound            #pip install playsound; https://www.delftstack.com/de/howto/python/python-play-mp3/
-import time
+import time                 #https://docs.python.org/3/library/time.html
+import os                   #https://rdrr.io/cran/installr/man/os.shutdown.html
+import threading            #https://docs.python.org/3/library/threading.html
 
 
 try:
 
-    #playsound.playsound('music.mp3')
-
     print("Shutdown PC in:")
 
+    #PLAY Outrosoundtrack
+    def music():
+        playsound.playsound('music.mp3')
 
-    count = 0
+    #COUNT FROM 15 to 1, than shutdown the pc
+    def consol_output():
+        count = 0
+        while count < 15:
+            time.sleep(1)
+            print(15 - count)
+            count = count + 1
+        print('BYE BYE')
 
-    while count < 13:
-        time.sleep(1)
-        print(13 - count)
-        count = count + 1
+    #SHUTDOWN the pc function
+    def shutdown():
+        os.shutdown(s=0, m=0, h=0)
+
+
+
+
+    threading.Thread(target = music).start()
+    threading.Thread(target =  consol_output).start()
 
 
 except Exception as err:
